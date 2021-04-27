0bd4edbcba5deb4a6e5de316c9fbe04f271db381
vonrimS <mr.mytab@gmail.com>
2021-04-27 23:33:25 +0300

    initial commit

---
+++ Lab01/Exercise1/Greeter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            Console.WriteLine("Enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Your name is {0}!", myName);
+        }
+    }
+}
