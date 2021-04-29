948ef26c807a7fafcf701f3e58493b6c69d7f58a
vonrimS <mr.mytab@gmail.com>
2021-04-29 12:54:32 +0300

    Exercise 1 and 2

---
+++ Lab02/Exercise1/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+
+    public enum AccountType { Checking, Deposit }
+
+    class Enum
+    {
+
+        static void Main(string[] args)
+        {
+            AccountType goldAccount, platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
---
+++ Lab02/Exercise2/Struct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+
+    // public enum AccountType { Checking, Deposit }; // this enum already exists within Lab02 namespace
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account Summary ***");
+
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+
+        }
+    }
+}
