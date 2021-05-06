c49cffc50c94326bf20bb22e952676f755cea997
vonrimS <mr.mytab@gmail.com>
2021-05-07 01:02:04 +0300

    Lab04 Exercise3 was made

---
+++ lab04/Exercise3/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04.Exercise3
+{
+    class Test
+    {
+
+        static void Main(string[] args)
+        {
+            //int x, y, greater;
+
+            //Console.WriteLine("Enter the first argument...");
+            //x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the second argument...");
+            //y = int.Parse(Console.ReadLine());
+
+            //greater = Utils.Greater(x, y);
+
+            //Console.WriteLine("According to your input, the greatest value is {0}", greater);
+
+            //Utils.Swap(ref x, ref y);
+
+            int f, x;
+            bool ok;
+
+            Console.WriteLine("\nEnter some integer for futher calculation:");
+            x = int.Parse(Console.ReadLine());
+
+            ok = Utils.Factorial(x, out f);
+
+            if (ok)
+                Console.WriteLine("Factorial(" + x + ") = " + f);
+            else
+                Console.WriteLine("Cannot compute this factorial");
+
+        }
+
+    }
+}
---
+++ lab04/Exercise3/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04.Exercise3
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp;
+            Console.WriteLine("BEFORE swapping we got values a = {0} and b = {1}", a, b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("...and AFTER swapping we got values a = {0} and b = {1}", a, b);
+        }
+
+        public static bool Factorial(int n, out int answer)
+        {
+            int k;
+            int f;
+            bool ok = true;
+            if (n < 0)
+                ok = false;
+            try
+            {
+                checked
+                {
+                    f = 1;
+                    for (k = 2; k <= n; k++)
+                    {
+                        f = f * k;
+                    }
+                }
+
+            }
+            catch (OverflowException)
+            {
+                throw new OverflowException();
+                f =0;
+                ok = false;
+            }
+                answer = f;
+                return ok;
+
+        }
+    }
+}
