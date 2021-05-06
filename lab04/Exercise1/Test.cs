894982b49b85bf486b47daa71a4566e33a596d67
vonrimS <mr.mytab@gmail.com>
2021-05-07 00:04:58 +0300

    Lab04 Exercise1 was made

---
+++ lab04/Exercise1/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Test
+    {
+
+        static void Main(string[] args)
+        {
+            int x, y, greater;
+
+            Console.WriteLine("Enter the first argument...");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second argument...");
+            y = int.Parse(Console.ReadLine());
+
+            greater = Utils.Greater(x, y);
+
+            Console.WriteLine("According to your input, the greatest value is {0}", greater);
+
+        }
+
+    }
+}
---
+++ lab04/Exercise1/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+
+        }
+    }
+}
