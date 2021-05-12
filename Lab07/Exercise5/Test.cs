bc0d43c96e8b3315144911859f1a198dcdcbd444
vonrimS <mr.mytab@gmail.com>
2021-05-13 00:20:30 +0300

    Lab07 Exercise5 was made

---
+++ Lab07/Exercise5/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Exercise5
+{
+    class Test
+    {
+
+        static void Main()
+        {
+            int i = 777;
+            string s = "Just some tiny string";
+            Coordinate c = new Coordinate(21.0, 68.0);
+
+            Utils.Display(i);
+            Utils.Display(s);
+            Utils.Display(c);
+
+        }
+    }
+}
---
+++ Lab07/Exercise5/Utils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Exercise5
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+
+        }
+
+        public static void Reverse(ref String s)
+        {
+            String sRev = "";
+
+            for (int i = (s.Length-1); i >= 0; i--)
+            {
+                sRev += s[i];
+            }
+
+            s = sRev;
+                           
+        }
+
+        public static bool IsItFormattable (object x)
+        {
+            if (x is System.IFormattable)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Display (object item)
+        {
+            IPrintable ip;
+
+            ip = (item as IPrintable);
+
+            if (ip != null)
+            {
+                ip.Print();
+            }
+            else
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+
+    }
+}
