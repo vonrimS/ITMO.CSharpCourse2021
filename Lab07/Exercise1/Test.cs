457aa42f7b7f06f81fcf178465fd8898f18af9f0
vonrimS <mr.mytab@gmail.com>
2021-05-12 22:23:30 +0300

    Lab07 Exercise1 was made

---
+++ Lab07/Exercise1/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Exercise1
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            BankAccount a = new BankAccount();
+            a.Populate(100);
+
+            BankAccount b = new BankAccount();
+            b.Populate(100);
+
+            a.GetInfo();
+            b.GetInfo();
+
+            Console.WriteLine("...$10 was transfered between accounts");
+            a.TransferFrom(b, 10);
+
+            a.GetInfo();
+            b.GetInfo();
+
+        }
+
+
+
+    }
+}
