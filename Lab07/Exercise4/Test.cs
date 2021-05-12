495f6fd30b2ab1c14f15daaafc8a3c5dfd0ca3dd
vonrimS <mr.mytab@gmail.com>
2021-05-12 23:47:18 +0300

    Lab07 Exercise4 was made

---
+++ Lab07/Exercise4/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Exercise4
+{
+    class Test
+    {
+
+        static void Main()
+        {
+            int i = 0;
+            ulong ul = 0;
+            string s = "";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(ul));
+            Console.WriteLine("string: {0}", Utils.IsItFormattable(s));
+
+        }
+    }
+}
