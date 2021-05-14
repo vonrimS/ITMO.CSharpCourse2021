b4dcbd159c016c04d117220a1f21e50c1eaecada
vonrimS <mr.mytab@gmail.com>
2021-05-15 02:21:19 +0300

    Lab08 Exercise2 was made

---
+++ Lab08/Exercise2/BankTransaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab08.Exercise2
+{
+    class BankTransaction
+    {
+
+        public BankTransaction(decimal tranAmount)
+        {
+            amount = tranAmount;
+            when = DateTime.Now;
+        }
+
+
+        private readonly decimal amount;
+        private readonly DateTime when;
+
+        public decimal Amount()
+        {
+            return amount;
+        }
+
+        public DateTime When()
+        {
+            return when;
+        }
+    }
+}
